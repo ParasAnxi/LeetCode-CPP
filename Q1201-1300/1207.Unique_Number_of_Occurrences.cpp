e0c2585cddc166dcdba843c31254ecd131c43731
ParasAnxi <paras.bhandari1010@gmail.com>
2023-01-31 20:19:13 +0530

    1207

--- Q1201-1300/1207.Unique_Number_of_Occurrences.cpp
+++ Q1201-1300/1207.Unique_Number_of_Occurrences.cpp
@@ -1,25 +1,23 @@
 class Solution {
 public:
     bool uniqueOccurrences(vector<int>& arr) {
-       vector<int> freq;
-        
+        int i = 0;
         sort(arr.begin(),arr.end());
-        for(int i=0;i<arr.size(); ){
-            int count =1;
-            for(int j=i+1;j<arr.size();j++){
-                if(arr[i]==arr[j]){
+        vector<int> freq;
+        while (i < arr.size()){
+            int count = 1;
+            for (int j = i+1; j< arr.size(); j++){
+                if (arr[i] == arr[j])
                     count++;
-                }
-                }
-                freq.push_back(count);
-                i=i+count;
             }
-        
-            sort(freq.begin(),freq.end());
-            for(int i=0;i<freq.size()-1;i++){
-                if(freq[i]!=freq[i+1])
-                    return true;
-            }
-         return false;
+            freq.push_back(count);
+            i = i + count;
         }
+        sort(freq.begin(),freq.end());
+        for (int i = 0; i < freq.size() - 1; i++){
+            if (freq[i] == freq[i+1])
+                return false;
+        }
+        return true;
+    }
 };

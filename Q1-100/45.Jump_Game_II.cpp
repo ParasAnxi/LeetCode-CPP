8f910b33b2d68bb10442da7b7bf7be49a9e3b6a6
ParasAnxi <paras.bhandari1010@gmail.com>
2023-02-08 13:02:18 +0530

    c

---
+++ Q1-100/45.Jump_Game_II.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+
+    int jump(vector<int>& nums) {
+
+      for(int i = 1; i < nums.size(); i++)
+      {
+        nums[i] = max(nums[i] + i, nums[i-1]);
+      }
+
+      int i = 0;
+      int ans = 0;
+
+      while(i < nums.size() - 1)
+      {
+        ans++;
+        i = nums[i];
+      }
+
+      return ans;
+    }
+};

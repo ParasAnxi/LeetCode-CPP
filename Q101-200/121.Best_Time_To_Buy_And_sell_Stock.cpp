b06123da25eb90c5f60351c59a79ba462edf86c5
ParasAnxi <paras.bhandari1010@gmail.com>
2023-02-25 09:38:15 +0530

    c

---
+++ Q101-200/121.Best_Time_To_Buy_And_sell_Stock.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+     int maxProfit(vector<int>& prices) {
+        int min_price = prices[0];
+        int max_pro = 0;
+        for (int i = 1; i < prices.size(); i++) {
+            max_pro = max(max_pro, prices[i] - min_price);
+            min_price = min(min_price, prices[i]);
+        }
+        return max_pro;
+    }
+};

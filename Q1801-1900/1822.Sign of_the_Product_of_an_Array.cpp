950af52c3f30a2da741ef5f29fe508aa1419bf87
ParasAnxi <paras.bhandari1010@gmail.com>
2023-05-02 11:12:18 +0530

    1822

---
+++ Q1801-1900/1822.Sign of_the_Product_of_an_Array.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int arraySign(vector<int>& nums) {
+        int p=0,n=0;
+        for(int i=0;i<nums.size();i++)
+        {
+            if(nums[i]>0)
+                p++;
+            else if(nums[i]<0)
+                n++;
+            else 
+                return 0;
+        }
+        if(n%2==0)
+            return 1;
+        return -1;
+    }
+};

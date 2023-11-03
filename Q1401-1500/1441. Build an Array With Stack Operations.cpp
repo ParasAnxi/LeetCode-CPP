56420f55c271884d19d6a34473ad7f3b2fe5dae0
ParasAnxi <paras.bhandari1010@gmail.com>
2023-11-03 09:30:11 +0530

    1441

---
+++ Q1401-1500/1441. Build an Array With Stack Operations.cpp
@@ -0,0 +1,28 @@
+class Solution
+{
+public:
+    vector<string> buildArray(vector<int> &target, int n)
+    {
+        ector<string> ans;
+        unordered_map<int, int> mp;
+        vector<int> temp;
+        for (auto it : target)
+            mp[it]++;
+        for (int i = 1; i <= n; i++)
+        {
+            if (temp == target)
+                break;
+            if (mp.find(i) == mp.end())
+            {
+                ans.push_back("Push");
+                ans.push_back("Pop");
+            }
+            else
+            {
+                temp.push_back(i);
+                ans.push_back("Push");
+            }
+        }
+        return ans;
+    }
+};

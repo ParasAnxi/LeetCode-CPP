8ac7750169c0551f7c4228025df4505888cc7c6a
ParasAnxi <paras.bhandari1010@gmail.com>
2023-11-20 11:06:12 +0530

    2391

---
+++ Q2301-2400/2391. Minimum Amount of Time to Collect Garbage.cpp
@@ -0,0 +1,44 @@
+class Solution
+{
+public:
+    int garbageCollection(vector<string> &garbage, vector<int> &travel)
+    {
+        int ans = 0;
+        int n = garbage.size();
+        int i = 0;
+        int dism = 0, disg = 0, disp = 0;
+        for (auto it : garbage)
+        {
+            string s = it;
+            for (int j = 0; j < s.size(); j++)
+            {
+                if (s[j] == 'M')
+                {
+                    ans++;
+                    ans += dism;
+                    dism = 0;
+                }
+                if (s[j] == 'G')
+                {
+                    ans++;
+                    ans += disg;
+                    disg = 0;
+                }
+                if (s[j] == 'P')
+                {
+                    ans++;
+                    ans += disp;
+                    disp = 0;
+                }
+            }
+            if (i != n - 1)
+            {
+                dism += travel[i];
+                disp += travel[i];
+                disg += travel[i];
+            }
+            i++;
+        }
+        return ans;
+    }
+};

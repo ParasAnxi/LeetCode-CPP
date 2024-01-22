f63f9e54b31d424c46b7db07448637d909b57783
ParasAnxi <paras.bhandari1010@gmail.com>
2024-01-22 17:27:50 +0530

    801

--- Q800-900/801. Minimum Swaps To Make Sequences Increasing.cpp
+++ Q800-900/801. Minimum Swaps To Make Sequences Increasing.cpp
@@ -77,6 +77,46 @@ public:
         }
         return dp[1][0];
     }
+    int so(vector<int> &nums1, vector<int> &nums2)
+    {
+        int n = nums1.size();
+        int swapp = 0;
+        int noSwap = 0;
+        int currSwap = 0;
+        int currNoSwap = 0;
+        for (int index = n - 1; index >= 1; index--)
+        {
+            for (int swapped = 1; swapped >= 0; swapped--)
+            {
+                int ans = INT_MAX;
+                int prev1 = nums1[index - 1];
+                int prev2 = nums2[index - 1];
+                if (swapped == 1)
+                {
+                    swap(prev1, prev2);
+                }
+                if (nums1[index] > prev1 && nums2[index] > prev2)
+                {
+                    ans = noSwap;
+                }
+                if (nums1[index] > prev2 && nums2[index] > prev1)
+                {
+                    ans = min(ans, 1 + swapp);
+                }
+                if (swapped)
+                {
+                    currSwap = ans;
+                }
+                else
+                {
+                    currNoSwap = ans;
+                }
+            }
+            swapp = currSwap;
+            noSwap = currNoSwap;
+        }
+        return min(swapp, noSwap);
+    }
     int minSwap(vector<int> &nums1, vector<int> &nums2)
     {
         nums1.insert(nums1.begin(), -1);
@@ -85,6 +125,6 @@ public:
         // return solve(nums1,nums2,1,swapped);
         vector<vector<int>> dp(nums1.size() + 1, vector<int>(2, -1));
         // return mem(nums1,nums2,1,swapped,dp);
-        return tab(nums1, nums2);
+        return so(nums1, nums2);
     }
 };

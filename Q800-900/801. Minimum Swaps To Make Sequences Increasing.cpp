7e80b580a20e3f009dac659188129fe790065bbe
ParasAnxi <paras.bhandari1010@gmail.com>
2024-01-22 17:14:37 +0530

    801

--- Q800-900/801. Minimum Swaps To Make Sequences Increasing.cpp
+++ Q800-900/801. Minimum Swaps To Make Sequences Increasing.cpp
@@ -24,11 +24,38 @@ public:
         }
         return ans;
     }
+    int mem(vector<int> &nums1, vector<int> &nums2, int index, bool swapped, vector<vector<int>> &dp)
+    {
+        if (index == nums1.size())
+        {
+            return 0;
+        }
+        if (dp[index][swapped] != -1)
+            return dp[index][swapped];
+        int ans = INT_MAX;
+        int prev1 = nums1[index - 1];
+        int prev2 = nums2[index - 1];
+        if (swapped == 1)
+        {
+            swap(prev1, prev2);
+        }
+        if (nums1[index] > prev1 && nums2[index] > prev2)
+        {
+            ans = mem(nums1, nums2, index + 1, 0, dp);
+        }
+        if (nums1[index] > prev2 && nums2[index] > prev1)
+        {
+            ans = min(ans, 1 + mem(nums1, nums2, index + 1, 1, dp));
+        }
+        return dp[index][swapped] = ans;
+    }
     int minSwap(vector<int> &nums1, vector<int> &nums2)
     {
         nums1.insert(nums1.begin(), -1);
         nums2.insert(nums2.begin(), -1);
         bool swapped = 0;
-        return solve(nums1, nums2, 1, swapped);
+        // return solve(nums1,nums2,1,swapped);
+        vector<vector<int>> dp(nums1.size() + 1, vector<int>(2, -1));
+        return mem(nums1, nums2, 1, swapped, dp);
     }
 };

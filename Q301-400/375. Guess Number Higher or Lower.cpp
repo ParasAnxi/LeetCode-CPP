c2162851d49c789e8b64f2941d2f72bf2aaff5ed
ParasAnxi <paras.bhandari1010@gmail.com>
2024-01-23 17:03:12 +0530

    375

--- Q301-400/375. Guess Number Higher or Lower.cpp
+++ Q301-400/375. Guess Number Higher or Lower.cpp
@@ -25,11 +25,35 @@ public:
         }
         return dp[s][e] = ans;
     }
-    int tab(int n);
+    int tab(int n)
+    {
+        vector<vector<int>> dp(n + 2, vector<int>(n + 2, 0));
+        for (int s = n; s >= 1; s--)
+        {
+            for (int e = s; e <= n; e++)
+            {
+                if (s == e)
+                {
+                    continue;
+                }
+                else
+                {
+                    int ans = INT_MAX;
+                    for (int i = s; i <= e; i++)
+                    {
+                        ans = min(ans, i + max(dp[s][i - 1], dp[i + 1][e]));
+                    }
+                    dp[s][e] = ans;
+                }
+            }
+        }
+        return dp[1][n];
+    }
     int getMoneyAmount(int n)
     {
         // return solve(1,n);
-        vector<vector<int>> dp(n + 1, vector<int>(n + 1, -1));
-        return mem(1, n, dp);
+        // vector<vector<int>>dp(n+1,vector<int>(n+1,-1));
+        // return mem(1,n,dp);
+        return tab(n);
     }
 };

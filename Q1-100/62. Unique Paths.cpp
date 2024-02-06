481a9a6d8de12e072e325a62724383e9f82d7e6a
ParasAnxi <paras.bhandari1010@gmail.com>
2024-02-06 16:21:04 +0530

    62

--- Q1-100/62. Unique Paths.cpp
+++ Q1-100/62. Unique Paths.cpp
@@ -1,17 +1,72 @@
 class Solution
 {
 public:
-    int uniquePaths(int m, int n)
+    int solve(int m, int n, int i, int j, vector<vector<int>> &dp)
     {
-        vector<vector<int>> dp(m + 1, vector<int>(n + 1, 0));
-        dp[0][1] = 1;
-        for (int i = 1; i <= m; i++)
+        if (i == m - 1 && j == n - 1)
         {
-            for (int j = 1; j <= n; j++)
+            return 1;
+        }
+        if (dp[i][j] != -1)
+            return dp[i][j];
+        int down = 0;
+        int right = 0;
+        if (i + 1 < m)
+            down = solve(m, n, i + 1, j, dp);
+        if (j + 1 < n)
+            right = solve(m, n, i, j + 1, dp);
+        return dp[i][j] = down + right;
+    }
+    int tab(int m, int n)
+    {
+        vector<vector<int>> dp(m, vector<int>(n, 0));
+        dp[m - 1][n - 1] = 1;
+        for (int i = m - 1; i >= 0; i--)
+        {
+            for (int j = n - 1; j >= 0; j--)
             {
-                dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
+                if (i == m - 1 && j == n - 1)
+                {
+                    continue;
+                }
+                int down = 0;
+                int right = 0;
+                if (i + 1 < m)
+                    down = dp[i + 1][j];
+                if (j + 1 < n)
+                    right = dp[i][j + 1];
+                dp[i][j] = down + right;
             }
         }
-        return dp[m][n];
+        return dp[0][0];
+    }
+    int so(int m, int n)
+    {
+        vector<int> dp(n, 0);
+        dp[n - 1] = 1;
+        for (int i = m - 1; i >= 0; i--)
+        {
+            for (int j = n - 1; j >= 0; j--)
+            {
+                if (i == m - 1 && j == n - 1)
+                {
+                    continue;
+                }
+                int down = 0;
+                int right = 0;
+                if (i + 1 < m)
+                    down = dp[j];
+                if (j + 1 < n)
+                    right = dp[j + 1];
+                dp[j] = down + right;
+            }
+        }
+        return dp[0];
+    }
+    int uniquePaths(int m, int n)
+    {
+        vector<vector<int>> dp(m, vector<int>(n, -1));
+        // return solve(m,n,0,0,dp);
+        return so(m, n);
     }
 };

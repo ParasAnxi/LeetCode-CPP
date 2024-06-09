e413111eec54a0f34a7cdf392759f48f5638c2f0
ParasAnxi <paras.bhandari1010@gmail.com>
2024-06-09 11:19:33 +0530

    974

--- Q901-1000/974.Subarray_Sums_Divisible_by_K.cpp
+++ Q901-1000/974.Subarray_Sums_Divisible_by_K.cpp
@@ -15,3 +15,29 @@ public:
         return ans;
     }
 };
+class Solution
+{
+public:
+    int subarraysDivByK(vector<int> &nums, int k)
+    {
+        unordered_map<int, int> mp;
+        mp.insert({0, 1});
+        int ans = 0;
+        int sum = 0;
+        for (int i = 0; i < nums.size(); i++)
+        {
+            sum += nums[i];
+            int rem = sum % k;
+            if (rem < 0)
+            {
+                rem = rem + k;
+            }
+            if (mp.find(rem) != mp.end())
+            {
+                ans += mp[rem];
+            }
+            mp[rem]++;
+        }
+        return ans;
+    }
+};

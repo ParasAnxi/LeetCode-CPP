2614ceb457894039ef3022879e32c574d08c1bb4
ParasAnxi <paras.bhandari1010@gmail.com>
2024-07-01 22:45:24 +0530

    1550

---
+++ Q1501-1600/1550. Three Consecutive Odds.cpp
@@ -0,0 +1,31 @@
+class Solution
+{
+public:
+    bool threeConsecutiveOdds(vector<int> &arr)
+    {
+        for (int i = 0; i < arr.size(); i++)
+        {
+            if (arr[i] % 2 == 0)
+            {
+                arr[i] = 0;
+            }
+            else
+                arr[i] = 1;
+        }
+        if (arr.size() < 3)
+        {
+            return false;
+        }
+
+        for (int i = 0; i <= arr.size() - 3; i++)
+        {
+            int sum = 0;
+            sum = arr[i] + arr[i + 1] + arr[i + 2];
+            if (sum == 3)
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+};

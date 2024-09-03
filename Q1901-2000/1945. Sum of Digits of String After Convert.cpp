c9e59684401c41ecb989ecb1250bd56f5476bc20
Paras Bhandari <paras.bhandari7010@gmail.com>
2024-09-03 09:58:30 +0530

    1945

---
+++ Q1901-2000/1945. Sum of Digits of String After Convert.cpp
@@ -0,0 +1,36 @@
+class Solution
+{
+public:
+    int solve(int num)
+    {
+        int sum = 0;
+        while (num > 0)
+        {
+            int digit = num % 10;
+            sum += digit;
+            num /= 10;
+        }
+        return sum;
+    }
+    int getLucky(string s, int k)
+    {
+        string str;
+        for (auto &c : s)
+        {
+            str += to_string((c - 'a') + 1);
+        }
+
+        int sum = 0;
+        for (auto &digits : str)
+        {
+            sum += digits - '0';
+        }
+
+        for (int i = 1; i < k; i++)
+        {
+            sum = solve(sum);
+        }
+
+        return sum;
+    }
+};

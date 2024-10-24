f8b9e9b584838757e32a2df9118b2f620d9a8203
Paras Bhandari <paras.bhandari7010@gmail.com>
2024-10-24 21:15:46 +0530

    951

---
+++ Q901-1000/951. Flip Equivalent Binary Trees.cpp
@@ -0,0 +1,40 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution
+{
+public:
+    bool flipEquiv(TreeNode *root1, TreeNode *root2)
+    {
+
+        // base case
+        if (root1 == NULL && root2 == NULL)
+        {
+            return true;
+        }
+        if ((root1 == NULL) || (root2 == NULL))
+        {
+            return false;
+        }
+        if (root1->val == root2->val)
+        {
+            bool isGood = flipEquiv(root1->left, root2->left) &&
+                          flipEquiv(root1->right, root2->right);
+            bool isBad = flipEquiv(root1->left, root2->right) &&
+                            flipEquiv(root1->right, root2->left);
+            return isGood || isBad;
+        }
+        else
+        {
+            return false;
+        }
+    }
+};

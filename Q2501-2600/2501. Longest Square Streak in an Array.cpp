9856af3f38293c063d362798d765aeb1c2ed2328
Paras Bhandari <paras.bhandari7010@gmail.com>
2024-10-28 11:14:20 +0530

    2501

--- Q2501-2600/2501. Longest Square Streak in an Array.cpp
+++ Q2501-2600/2501. Longest Square Streak in an Array.cpp
@@ -26,3 +26,31 @@ public:
         return ans;
     }
 };
+
+class Solution
+{
+public:
+    int longestSquareStreak(vector<int> &nums)
+    {
+        int n = nums.size();
+        set<long long> s;
+        for (auto i : nums)
+        {
+            s.insert(i);
+        }
+        int ans = -1;
+        for (int i = 0; i < nums.size(); i++)
+        {
+            int count = 1;
+            long long mul = pow(nums[i], 2);
+            while (s.count(mul))
+            {
+                count++;
+                mul = pow(mul, 2);
+            }
+            if (count > 1)
+                ans = max(ans, count);
+        }
+        return ans;
+    }
+};

245371fbcb4700e04c00f35389cbe6d214179913
Paras Bhandari <paras.bhandari7010@gmail.com>
2025-03-02 21:15:21 +0530

    2570

---
+++ Q2701-2800/2570. Merge Two 2D Arrays by Summing Values.cpp
@@ -0,0 +1,30 @@
+class Solution
+{
+public:
+    vector<vector<int>> mergeArrays(vector<vector<int>> &nums1, vector<vector<int>> &nums2)
+    {
+        vector<vector<int>> ans;
+        map<int, int> mp;
+
+        for (auto &i : nums1)
+        {
+            if (!mp[&i[0]])
+                mp[&i[0]] = &i[1];
+            else
+                mp[&i[0]] += &i[1];
+        }
+        for (auto &i : nums2)
+        {
+            if (!mp[&i[0]])
+                mp[&i[0]] = &i[1];
+            else
+                mp[&i[0]] += &i[1];
+        }
+
+        for (auto &i : mp)
+        {
+            ans.push_back({&i.first, &i.second});
+        }
+        return ans;
+    }
+};

cb94749555bfe5f6e1b1d91fb59ec47fb90922ac
Paras Bhandari <paras.bhandari7010@gmail.com>
2025-04-20 09:09:21 +0530

    3452

---
+++ Q3401-3500/3452. Sum of Good Numbers.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+  int sumOfGoodNumbers(vector<int> &nums, int k) {
+    int ans = 0;
+    for (int i = 0; i < nums.size(); i++) {
+      bool flag = true;
+      if (i - k >= 0 && nums[i] <= nums[i - k]) {
+        flag = false;
+      }
+      if (i + k < nums.size() && nums[i] <= nums[i + k]) {
+        flag = false;
+      }
+      if (flag) {
+        ans += nums[i];
+      }
+    }
+    return ans;
+  }
+};

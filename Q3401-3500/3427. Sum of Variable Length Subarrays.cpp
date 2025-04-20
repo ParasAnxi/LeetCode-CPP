0e1e630fedd7d821201c6eea7e676f105c6119fb
Paras Bhandari <paras.bhandari7010@gmail.com>
2025-04-20 09:31:54 +0530

    3427

--- Q3401-3500/3427. Sum of Variable Length Subarrays.cpp
+++ Q3401-3500/3427. Sum of Variable Length Subarrays.cpp
@@ -1,16 +1,22 @@
+using vi = vector<int>;
+#define pb push_back
 class Solution {
 public:
+  vi build(vi &nums) {
+    vi tree = {nums[0]};
+    const int size = nums.size();
+    for (int i = 0; i < size; i++) {
+      tree.pb(nums[i] + tree[tree.size() - 1]);
+    }
+    return tree;
+  }
   int subarraySum(vector<int> &nums) {
-    int n = nums.size();
     int ans = 0;
-    int i = 0;
-    for (int i = 0; i < n; i++) {
-
-      i = max(0, i - nums[i]);
-
-      for (int j = i; j <= i; j++) {
-        ans += nums[j];
-      }
+    vi tree = build(nums);
+    const int size = nums.size();
+    for (int i = 0; i < size; i++) {
+      int start = max(0, i - nums[i]);
+      ans += tree[i] - tree[start] + nums[i];
     }
     return ans;
   }

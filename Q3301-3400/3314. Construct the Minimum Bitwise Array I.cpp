11d5d814792bb1e9f88ec5eeb378a684185c71b8
Paras Bhandari <paras.bhandari7010@gmail.com>
2025-04-21 11:19:49 +0530

    3314

---
+++ Q3301-3400/3314. Construct the Minimum Bitwise Array I.cpp
@@ -0,0 +1,17 @@
+using vi = vector<int>;
+class Solution {
+public:
+  vector<int> minBitwiseArray(vector<int> &nums) {
+    int n = nums.size();
+    vi ans(n, -1);
+    for (int i = 0; i < n; i++) {
+      for (int j = 0; j < nums[i]; j++) {
+        if ((j | (j + 1)) == nums[i]) {
+          ans[i] = j;
+          break;
+        }
+      }
+    }
+    return ans;
+  }
+};

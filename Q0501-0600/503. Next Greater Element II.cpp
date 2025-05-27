0a8a49ef2c46902cf9c2e5813e58c2b64b540a8f
Paras Bhandari <paras.bhandari7010@gmail.com>
2025-05-27 09:37:41 +0530

    503

---
+++ Q0501-0600/503. Next Greater Element II.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+  vector<int> nextGreaterElements(vector<int> &nums) {
+    int n = nums.size();
+    stack<int> s;
+    vector<int> ans(n, -1);
+    for (int i = 2 * n - 1; i >= 0; i--) {
+      int curr = nums[i % n];
+      while (!s.empty() && s.top() <= curr) {
+        s.pop();
+      }
+
+      if (i < n) {
+        ans[i] = s.empty() ? -1 : s.top();
+      }
+      s.push(curr);
+    }
+    return ans;
+  }
+};

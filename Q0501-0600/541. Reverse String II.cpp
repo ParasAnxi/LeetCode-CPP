5b8f6c58532d9b5c9de2e63a5b170a8f24cc2536
Paras Bhandari <paras.bhandari7010@gmail.com>
2025-05-30 09:09:49 +0530

    541

---
+++ Q0501-0600/541. Reverse String II.cpp
@@ -0,0 +1,40 @@
+using ll = long long;
+using ull = unsigned long long;
+using ld = long double;
+using cd = complex<double>;
+using pii = pair<int, int>;
+using psi = pair<string, int>;
+using pll = pair<ll, ll>;
+using ppii = pair<int, pii>;
+using vi = vector<int>;
+using vd = vector<double>;
+using vvi = vector<vi>;
+using vvvi = vector<vvi>;
+using vll = vector<ll>;
+using vvll = vector<vll>;
+using vc = vector<char>;
+using vpii = vector<pii>;
+using vpll = vector<pll>;
+using pis = pair<int, string>;
+using vs = vector<string>;
+using mii = map<int, int>;
+using mll = map<ll, ll>;
+using umii = unordered_map<int, int>;
+using umcvi = unordered_map<char, vi>;
+using umci = unordered_map<char, int>;
+#define pb push_back
+#define MOD 1000000007
+#define LMOD 1000000000000000007LL
+class Solution {
+public:
+  string reverseStr(string s, int k) {
+    int st = 0;
+    int e = min(k, (int)s.length());
+    while (st < s.length()) {
+      reverse(s.begin() + st, s.begin() + e);
+      st += 2 * k;
+      e = min(st + k, (int)s.length());
+    }
+    return s;
+  }
+};

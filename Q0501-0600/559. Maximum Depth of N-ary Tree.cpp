0b8f67661206baf0efa78b3bd2a2423a8466ae2e
Paras Bhandari <paras.bhandari7010@gmail.com>
2025-05-31 09:24:15 +0530

    559

---
+++ Q0501-0600/559. Maximum Depth of N-ary Tree.cpp
@@ -0,0 +1,59 @@
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    vector<Node*> children;
+
+    Node() {}
+
+    Node(int _val) {
+        val = _val;
+    }
+
+    Node(int _val, vector<Node*> _children) {
+        val = _val;
+        children = _children;
+    }
+};
+*/
+using ll = long long;
+using ull = unsigned long long;
+using ld = long double;
+using cd = complex<double>;
+using pii = pair<int, int>;
+using psi = pair<string, int>;
+using pll = pair<ll, ll>;
+using ppii = pair<int, pii>;
+using vi = vector<int>;
+using vd = vector<double>;
+using vvi = vector<vi>;
+using vvvi = vector<vvi>;
+using vll = vector<ll>;
+using vvll = vector<vll>;
+using vc = vector<char>;
+using vpii = vector<pii>;
+using vpll = vector<pll>;
+using pis = pair<int, string>;
+using vs = vector<string>;
+using mii = map<int, int>;
+using mll = map<ll, ll>;
+using umii = unordered_map<int, int>;
+using umcvi = unordered_map<char, vi>;
+using umci = unordered_map<char, int>;
+#define pb push_back
+#define MOD 1000000007
+#define LMOD 1000000000000000007LL
+class Solution {
+public:
+  int solve(Node *root) {
+    if (!root)
+      return 0;
+    int ans = 0;
+    for (auto &i : root->children) {
+      ans = max(ans, solve(i));
+    }
+    return 1 + ans;
+  }
+  int maxDepth(Node *root) { return solve(root); }
+};

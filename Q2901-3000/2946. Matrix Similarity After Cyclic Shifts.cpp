75fb20379c77152e7c91906f0bf66bbfbe7eb92f
Paras Bhandari <paras.bhandari7010@gmail.com>
2026-03-27 20:58:48 +0530

    2946

---
+++ Q2901-3000/2946. Matrix Similarity After Cyclic Shifts.cpp
@@ -0,0 +1,45 @@
+using ll = long long;
+using ull = unsigned long long;
+using ld = long double;
+using cd = complex<double>;
+using pii = pair<int, int>;
+using psi = pair<string, int>;
+using pll = pair<ll, ll>;
+using ppii = pair<int, pii>;
+using vi = vector<int>;
+using vd = vector<double>;
+using vvi = vector<vi>;
+using vvvi = vector<vvi>;
+using vll = vector<ll>;
+using vvll = vector<vll>;
+using vc = vector<char>;
+using vpii = vector<pii>;
+using vpll = vector<pll>;
+using pis = pair<int, string>;
+using vs = vector<string>;
+using mii = map<int, int>;
+using mll = map<ll, ll>;
+using pli = pair<ll, int>;
+using umii = unordered_map<int, int>;
+using umcvi = unordered_map<char, vi>;
+using umsi = unordered_map<string, int>;
+using mivi = map<int, vi>;
+using umci = unordered_map<char, int>;
+#define pb push_back
+#define MOD 1000000007
+#define LMOD 1000000000000000007LL
+class Solution {
+public:
+  bool areSimilar(vector<vector<int>> &mat, int k) {
+    int n = mat.size();
+    int m = mat[0].size();
+    for (int i = 0; i < n; i++) {
+      for (int j = 0; j < m; j++) {
+        if (mat[i][j] != mat[i][(j + k) % m]) {
+          return false;
+        }
+      }
+    }
+    return true;
+  }
+};

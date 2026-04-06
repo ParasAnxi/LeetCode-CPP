b9077deb827a771c0a217b1d9ca964ffee22a41c
Paras Bhandari <paras.bhandari7010@gmail.com>
2026-04-06 13:08:00 +0530

    3880

---
+++ Q3801-3900/3880. Minimum Absolute Difference Between Two Values.cpp
@@ -0,0 +1,49 @@
+using ll = long long;
+using ull = unsigned long long;
+using ld = long double;
+using cd = complex<double>;
+using pii = pair<int, int>;
+using psi = pair<string, int>;
+using pll = pair<ll, ll>;
+using ppii = pair<int, pii>;
+using vi = vector<int>;
+using vd = vector<double>;
+using vvi = vector<vi>;
+using vvvi = vector<vvi>;
+using vll = vector<ll>;
+using vvll = vector<vll>;
+using vc = vector<char>;
+using vpii = vector<pii>;
+using vpll = vector<pll>;
+using pis = pair<int, string>;
+using vs = vector<string>;
+using mii = map<int, int>;
+using mll = map<ll, ll>;
+using pli = pair<ll, int>;
+using umii = unordered_map<int, int>;
+using umcvi = unordered_map<char, vi>;
+using umsi = unordered_map<string, int>;
+using mivi = map<int, vi>;
+using umci = unordered_map<char, int>;
+#define pb push_back
+#define MOD 1000000007
+#define LMOD 1000000000000000007LL
+class Solution {
+public:
+  int minAbsoluteDifference(vector<int> &nums) {
+    int s = -1;
+    int e = -1;
+    int ans = INT_MAXI;
+    for (int i = 0; i < nums.size(); i++) {
+      if (nums[i] == 1) {
+        s = i;
+      } else if (nums[i] == 2) {
+        e = i;
+      }
+      if (s != -1 and e != -1) {
+        ans = min(ans, abs(s - e));
+      }
+    }
+    return ans == INT_MAXI ? -1 : ans;
+  }
+};

b167fe6275189aeaad4dc7bee06a9c48cc76a417
Paras Bhandari <paras.bhandari7010@gmail.com>
2026-05-01 10:55:56 +0530

    396

--- Q0301-0400/396. Rotate Function.cpp
+++ Q0301-0400/396. Rotate Function.cpp
@@ -1,3 +1,33 @@
+using ll = long long;
+using ull = unsigned long long;
+using ld = long double;
+using cd = complex<double>;
+using pii = pair<int, int>;
+using psi = pair<string, int>;
+using pll = pair<ll, ll>;
+using ppii = pair<int, pii>;
+using vi = vector<int>;
+using vd = vector<double>;
+using vvi = vector<vi>;
+using vvvi = vector<vvi>;
+using vll = vector<ll>;
+using vvll = vector<vll>;
+using vc = vector<char>;
+using vpii = vector<pii>;
+using vpll = vector<pll>;
+using pis = pair<int, string>;
+using vs = vector<string>;
+using mii = map<int, int>;
+using mll = map<ll, ll>;
+using pli = pair<ll, int>;
+using umii = unordered_map<int, int>;
+using umcvi = unordered_map<char, vi>;
+using umsi = unordered_map<string, int>;
+using mivi = map<int, vi>;
+using umci = unordered_map<char, int>;
+#define pb push_back
+#define MOD 1000000007
+#define LMOD 1000000000000000007LL
 class Solution {
 public:
   int maxRotateFunction(vector<int> &nums) {

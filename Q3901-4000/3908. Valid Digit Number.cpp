4e76f153a63fa966c83814500282d9f054b25c0e
Paras Bhandari <paras.bhandari7010@gmail.com>
2026-05-03 08:28:14 +0530

    3908

---
+++ Q3901-4000/3908. Valid Digit Number.cpp
@@ -0,0 +1,47 @@
+using ll = long long;
+using ull = unsigned long long;
+using ld = long double;
+using cd = complex<double>;
+using pii = pair<int, int>;
+using psi = pair<string, int>;
+using pll = pair<ll, ll>;
+using ppii = pair<int, pii>;
+using vi = vector<int>;
+using vd = vector<double>;
+using vvi = vector<vi>;
+using vvvi = vector<vvi>;
+using vll = vector<ll>;
+using vvll = vector<vll>;
+using vc = vector<char>;
+using vpii = vector<pii>;
+using vpll = vector<pll>;
+using pis = pair<int, string>;
+using vs = vector<string>;
+using mii = map<int, int>;
+using mll = map<ll, ll>;
+using pli = pair<ll, int>;
+using umii = unordered_map<int, int>;
+using umcvi = unordered_map<char, vi>;
+using umsi = unordered_map<string, int>;
+using mivi = map<int, vi>;
+using umci = unordered_map<char, int>;
+#define pb push_back
+#define MOD 1000000007
+#define LMOD 1000000000000000007LL
+class Solution {
+public:
+  bool validDigit(int n, int x) {
+    int z = 0, i = 0;
+    while (n) {
+      if (n < 10 and n != x) {
+        i = 1;
+      }
+      int d = n % 10;
+      if (d == x) {
+        z++;
+      }
+      n /= 10;
+    }
+    return z && i;
+  }
+};

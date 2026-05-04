375ab8af4edce8de8544a99ba766e06fedd95e72
Paras Bhandari <paras.bhandari7010@gmail.com>
2026-05-04 14:12:20 +0530

    48

--- Q0001-0100/48. Rotate Image.cpp
+++ Q0001-0100/48. Rotate Image.cpp
@@ -1,13 +1,41 @@
+using ll = long long;
+using ull = unsigned long long;
+using ld = long double;
+using cd = complex<double>;
+using pii = pair<int, int>;
+using psi = pair<string, int>;
+using pll = pair<ll, ll>;
+using ppii = pair<int, pii>;
+using vi = vector<int>;
+using vd = vector<double>;
+using vvi = vector<vi>;
+using vvvi = vector<vvi>;
+using vll = vector<ll>;
+using vvll = vector<vll>;
+using vc = vector<char>;
+using vpii = vector<pii>;
+using vpll = vector<pll>;
+using pis = pair<int, string>;
+using vs = vector<string>;
+using mii = map<int, int>;
+using mll = map<ll, ll>;
+using umii = unordered_map<int, int>;
+using umcvi = unordered_map<char, vi>;
+using umci = unordered_map<char, int>;
+#define pb push_back
+#define MOD 1000000007
+#define LMOD 1000000000000000007LL
 class Solution {
 public:
-    void rotate(vector<vector<int>>& matrix) {
-        for(int i = 0; i<matrix.size();i++){
-            for(int j = 0 ; j<i; j++){
-                swap(matrix[i][j],matrix[j][i]);
-            }
-        }
-        for(auto &i : matrix){
-            reverse(i.begin(),i.end());
-        }
+  void rotate(vector<vector<int>> &matrix) {
+    int n = matrix.size();
+    for (int i = 0; i < n; i++) {
+      for (int j = 0; j < i; j++) {
+        swap(matrix[i][j], matrix[j][i]);
+      }
     }
+    for (auto &i : matrix) {
+      reverse(i.begin(), i.end());
+    }
+  }
 };

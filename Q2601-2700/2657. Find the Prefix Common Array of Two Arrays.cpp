8159beba3a69d99f8713f5a929d4564fb899525f
Paras Bhandari <paras.bhandari7010@gmail.com>
2026-05-21 08:53:36 +0530

    2657

--- Q2601-2700/2657. Find the Prefix Common Array of Two Arrays.cpp
+++ Q2601-2700/2657. Find the Prefix Common Array of Two Arrays.cpp
@@ -1,25 +1,51 @@
-class Solution
-{
+using ll = long long;
+using ull = unsigned long long;
+using ld = long double;
+using cd = complex<double>;
+using pii = pair<int, int>;
+using psi = pair<string, int>;
+using pll = pair<ll, ll>;
+using ppii = pair<int, pii>;
+using vi = vector<int>;
+using vd = vector<double>;
+using vvi = vector<vi>;
+using vvvi = vector<vvi>;
+using vll = vector<ll>;
+using vvll = vector<vll>;
+using vc = vector<char>;
+using vpii = vector<pii>;
+using vpll = vector<pll>;
+using pis = pair<int, string>;
+using vs = vector<string>;
+using mii = map<int, int>;
+using mll = map<ll, ll>;
+using pli = pair<ll, int>;
+using umii = unordered_map<int, int>;
+using umcvi = unordered_map<char, vi>;
+using umsi = unordered_map<string, int>;
+using mivi = map<int, vi>;
+using umci = unordered_map<char, int>;
+#define pb push_back
+#define MOD 1000000007
+#define LMOD 1000000000000000007LL
+class Solution {
 public:
-    vector<int> findThePrefixCommonArray(vector<int> &A, vector<int> &B)
-    {
-        int n = A.size();
-        vector<int> freq(n, 0);
-        int c = 0;            
-        vector<int> ans(n);
-
-        for (int i = 0; i < n; i++)
-        {
-            freq[A[i] - 1] += 1;
-            if (freq[A[i] - 1] == 2)
-                c++;
-
-            freq[B[i] - 1] += 1;
-            if (freq[B[i] - 1] == 2)
-                c++;
-
-            ans[i] = c;
-        }
-        return ans;
+  vector<int> findThePrefixCommonArray(vector<int> &A, vector<int> &B) {
+    int n = A.size();
+    vi vF(n, 0);
+    int cnt = 0;
+    vi ans(n);
+    for (int i = 0; i < n; i++) {
+      vF[A[i] - 1] += 1;
+      if (vF[A[i] - 1] == 2) {
+        cnt++;
+      }
+      vF[B[i] - 1] += 1;
+      if (vF[B[i] - 1] == 2) {
+        cnt++;
+      }
+      ans[i] = cnt;
     }
+    return ans;
+  }
 };

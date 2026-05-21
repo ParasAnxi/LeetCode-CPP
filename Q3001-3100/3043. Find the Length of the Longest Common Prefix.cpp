5969c253d3f4dd6a63cd113332fc797960af2764
Paras Bhandari <paras.bhandari7010@gmail.com>
2026-05-21 08:53:44 +0530

    3043

--- Q3001-3100/3043. Find the Length of the Longest Common Prefix.cpp
+++ Q3001-3100/3043. Find the Length of the Longest Common Prefix.cpp
@@ -1,31 +1,55 @@
-class Solution
-{
+using ll = long long;
+using ull = unsigned long long;
+using ld = long double;
+using cd = complex<double>;
+using pii = pair<int, int>;
+using psi = pair<string, int>;
+using pll = pair<ll, ll>;
+using ppii = pair<int, pii>;
+using vi = vector<int>;
+using vd = vector<double>;
+using vvi = vector<vi>;
+using vvvi = vector<vvi>;
+using vll = vector<ll>;
+using vvll = vector<vll>;
+using vc = vector<char>;
+using vpii = vector<pii>;
+using vpll = vector<pll>;
+using pis = pair<int, string>;
+using vs = vector<string>;
+using mii = map<int, int>;
+using mll = map<ll, ll>;
+using pli = pair<ll, int>;
+using umii = unordered_map<int, int>;
+using umcvi = unordered_map<char, vi>;
+using umsi = unordered_map<string, int>;
+using mivi = map<int, vi>;
+using umci = unordered_map<char, int>;
+#define pb push_back
+#define MOD 1000000007
+#define LMOD 1000000000000000007LL
+class Solution {
 public:
-    int longestCommonPrefix(vector<int> &arr1, vector<int> &arr2)
-    {
-        unordered_set<string> st;
-        for (auto x : arr1)
-        {
-            string str = to_string(x);
-            string s;
-            for (int i = 0; i < str.size(); i++)
-            {
-                s += str[i];
-                st.insert(s);
-            }
-        }
-        int length = 0;
-        for (auto x : arr2)
-        {
-            string str = to_string(x);
-            string s;
-            for (int i = 0; i < str.size(); i++)
-            {
-                s += str[i];
-                if (st.find(s) != st.end() && s.size() > length)
-                    length = s.size();
-            }
-        }
-        return length;
+  int longestCommonPrefix(vector<int> &arr1, vector<int> &arr2) {
+    unordered_set<string> st;
+    for (auto i : arr1) {
+      string str = to_string(i);
+      string s;
+      for (int i = 0; i < str.size(); i++) {
+        s += str[i];
+        st.insert(s);
+      }
     }
+    int len = 0;
+    for (auto i : arr2) {
+      string str = to_string(i);
+      string s;
+      for (int i = 0; i < str.size(); i++) {
+        s += str[i];
+        if (st.find(s) != st.end() && s.size() > len)
+          len = s.size();
+      }
+    }
+    return len;
+  }
 };

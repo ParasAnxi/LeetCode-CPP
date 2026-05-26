fd847752d9843b3be565de29ab4e243e45b51a44
Paras Bhandari <paras.bhandari7010@gmail.com>
2026-05-26 21:31:56 +0530

    3788

---
+++ Q3701-3800/3788. Maximum Score of a Split.cpp
@@ -0,0 +1,46 @@
+using ll = long long;
+using ull = unsigned long long;
+using ld = long double;
+using cd = complex<double>;
+using pii = pair<int, int>;
+using psi = pair<string, int>;
+using pll = pair<ll, ll>;
+using ppii = pair<int, pii>;
+using vi = vector<int>;
+using vd = vector<double>;
+using vvi = vector<vi>;
+using vvvi = vector<vvi>;
+using vll = vector<ll>;
+using vvll = vector<vll>;
+using vc = vector<char>;
+using vpii = vector<pii>;
+using vpll = vector<pll>;
+using pis = pair<int, string>;
+using vs = vector<string>;
+using mii = map<int, int>;
+using mll = map<ll, ll>;
+using pli = pair<ll, int>;
+using umii = unordered_map<int, int>;
+using umcvi = unordered_map<char, vi>;
+using umsi = unordered_map<string, int>;
+using mivi = map<int, vi>;
+using umci = unordered_map<char, int>;
+#define pb push_back
+#define MOD 1000000007
+#define LMOD 1000000000000000007LL
+class Solution {
+public:
+  long long maximumScore(vector<int> &nums) {
+    int n = nums.size();
+    ll sum = accumulate(nums.begin(), nums.end(), 0LL);
+    sum -= nums.back();
+    ll mini = nums.back();
+    ll ans = LLONG_MIN;
+    for (int i = n - 2; i >= 0; i--) {
+      ans = max(ans, sum - mini);
+      sum -= nums[i];
+      mini = min(mini, 1LL * nums[i]);
+    }
+    return ans;
+  }
+};

79531baeee42b2560678a280d8b81833e8f09495
Paras Bhandari <paras.bhandari7010@gmail.com>
2026-05-29 20:21:59 +0530

    3909

---
+++ Q3901-4000/3909. Compare Sums of Bitonic Parts.cpp
@@ -0,0 +1,52 @@
+using ll = long long;
+using ull = unsigned long long;
+using ld = long double;
+using cd = complex<double>;
+using pii = pair<int, int>;
+using psi = pair<string, int>;
+using pll = pair<ll, ll>;
+using ppii = pair<int, pii>;
+using vi = vector<int>;
+using vd = vector<double>;
+using vvi = vector<vi>;
+using vvvi = vector<vvi>;
+using vll = vector<ll>;
+using vvll = vector<vll>;
+using vc = vector<char>;
+using vpii = vector<pii>;
+using vpll = vector<pll>;
+using pis = pair<int, string>;
+using vs = vector<string>;
+using mii = map<int, int>;
+using mll = map<ll, ll>;
+using pli = pair<ll, int>;
+using umii = unordered_map<int, int>;
+using umcvi = unordered_map<char, vi>;
+using umsi = unordered_map<string, int>;
+using mivi = map<int, vi>;
+using umci = unordered_map<char, int>;
+#define pb push_back
+#define MOD 1000000007
+#define LMOD 1000000000000000007LL
+class Solution {
+public:
+  int compareBitonicSums(vector<int> &nums) {
+    int n = nums.size();
+    ll st = nums[0];
+    ll e = nums.back();
+    for (int i = 1; i < n; i++) {
+      if (nums[i - 1] < nums[i]) {
+        st += nums[i];
+      } else if (nums[i - 1] > nums[i]) {
+        e += nums[i - 1];
+      }
+    }
+    if (st > e) {
+      return 0;
+    } else if (st < e) {
+      return 1;
+    } else {
+      return -1;
+    }
+  }
+};

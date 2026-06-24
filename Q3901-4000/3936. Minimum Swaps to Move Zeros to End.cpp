151300e854ec75c9c2f0568727d834e6712b3731
Paras Bhandari <paras.bhandari7010@gmail.com>
2026-06-24 21:53:57 +0530

    3936

---
+++ Q3901-4000/3936. Minimum Swaps to Move Zeros to End.cpp
@@ -0,0 +1,51 @@
+using ll = long long;
+using ull = unsigned long long;
+using ld = long double;
+using cd = complex<double>;
+using pii = pair<int, int>;
+using psi = pair<string, int>;
+using pll = pair<ll, ll>;
+using ppii = pair<int, pii>;
+using vi = vector<int>;
+using vd = vector<double>;
+using vvi = vector<vi>;
+using vvvi = vector<vvi>;
+using vll = vector<ll>;
+using vvll = vector<vll>;
+using vc = vector<char>;
+using vpii = vector<pii>;
+using vpll = vector<pll>;
+using pis = pair<int, string>;
+using vs = vector<string>;
+using mii = map<int, int>;
+using mll = map<ll, ll>;
+using pli = pair<ll, int>;
+using umii = unordered_map<int, int>;
+using umcvi = unordered_map<char, vi>;
+using umsi = unordered_map<string, int>;
+using mivi = map<int, vi>;
+using umci = unordered_map<char, int>;
+#define pb push_back
+#define MOD 1000000007
+#define LMOD 1000000000000000007LL
+class Solution {
+public:
+  int minimumSwaps(vector<int> &nums) {
+    int cnt = 0;
+    for (auto i : nums) {
+      if (i == 0) {
+        cnt++;
+      }
+    }
+    int j = nums.size() - 1;
+    int x = 0;
+    int y = cnt;
+    while (y--) {
+      if (nums[j] == 0) {
+        x++;
+      }
+      j--;
+    }
+    return cnt - x;
+  }
+};
